9b0aeedb4d7201d1646176a032876370687a3e5b
Louis Leffler <louisleffler@gmail.com>
2020-11-28 22:47:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,6 +15,7 @@ def literal_aoh
     [{name: "Don Gately", occupation: "Live-in Staff"},
     {name: "Joelle van Dyne", occupation:"Radio Personality"},
     {name: "Kate Gompert", occupation: "None"},
+    {name:
     {name: "Bruce Green", occupation: "Fan of Mildred"}]
 end
 
