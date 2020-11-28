879405ad0d2948200d528cc5d2bf16bfb5096ae0
Louis Leffler <louisleffler@gmail.com>
2020-11-28 22:51:19 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,13 +14,12 @@ def literal_aoh
   literal_aoh = 
     [{name: "Don Gately", occupation: "Live-in Staff"},
     {name: "Joelle van Dyne", occupation:"Radio Personality"},
-    {name: "Pat Monteseian", occupation: "Staff"},
+    {name: "Pat Monteseian", occupation "Staff"},
     {name: "Kate Gompert", occupation: "None"},
     {name: "Bruce Green", occupation: "Fan of Mildred"}]
 end
 
 def aoh_lookup(aoh, row, key)
-  aoh_lookup[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
